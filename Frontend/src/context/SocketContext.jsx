a21be3934ca1acd6e8df4e2cc499d38e936c96df
div0364 <divyam.d.sinha@gmail.com>
2024-12-31 18:03:03 +0530

    ten commit

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chatapp-assl.onrender.com", {
+      const socket = io("https://chat-app-nrkz.vercel.app/", {
         query: {
           userId: authUser.user._id,
         },
