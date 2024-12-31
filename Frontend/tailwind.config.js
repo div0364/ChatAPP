fec0fe2fff7ddbc30219a85c778859245a0050ec
div0364 <divyam.d.sinha@gmail.com>
2024-12-31 17:42:46 +0530

    eight commit

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Include your source files
-  ],
-  theme: {
-    extend: {}, // Extend the default Tailwind theme if needed
-  },
-  plugins: [require("daisyui")], // Include the DaisyUI plugin
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
 };
